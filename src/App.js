012f32f207f362a20e7516debce694488034baec
emilysupercode <sheil.emily.sc@gmail.com>
2022-12-05 15:41:50 +0100

    user input field added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-
 function App() {
   const [weather, setWeather] = useState([]);
   const [cityName, setCityName] = useState("Berlin");
@@ -24,7 +23,8 @@ function App() {
   let updatedLocalTime = dateAccountForTimezone.toISOString().match(/\d\d:\d\d/);
 
   let weatherIconCode = weather.weather[0].icon;
-  // onClick={setCity(`Leipzig`)}
+  let userInputCityEl = document.getElementById("cityInput");
+
   return (
     <div className="App">
       <div className='wrapper'>
@@ -32,10 +32,13 @@ function App() {
         <button onClick={() => setCityName("München")}>München</button>
         <button onClick={() => setCityName("Wien")}>Wien</button>
         <button onClick={() => setCityName("Hamburg")}>Hamburg</button>
+        <br />
+        <input className="cityInput" name="cityInput" id='cityInput' type="text" placeholder='city name'></input>
+        <button onClick={() => setCityName(userInputCityEl.value)}>Find My City</button>
         <p className='weatherOverview'>{weather.weather[0].description} in {weather.name}</p>
         <img src={`http://openweathermap.org/img/wn/${weatherIconCode}@2x.png`} alt="current weather" />
         <p>Current: {weather.main.temp}°C</p>
-        <p>Wind speed: {weather.wind.speed} km/h</p>
+        <p>Wind speed: {weather.wind.speed} m/sec</p>
         <p>Local time: {updatedLocalTime}</p>
       </div>
     </div>
