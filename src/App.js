ab9346ae970d8b7bddfbd3168c87e7599d5eb8e4
emilysupercode <sheil.emily.sc@gmail.com>
2022-12-05 11:53:28 +0100

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [weather, setWeather] = useState([]);
+  const [cityName, setCityName] = useState("Berlin");
+
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&lang=de&appid=${process.env.REACT_APP_API_KEY}&units=metric`)
+      .then(res => res.json())
+      .then((weather) => {
+        setWeather(weather)
+      })
+  }, [cityName])
+
+  if (weather.weather === undefined) {
+    return;
+  }
+
+  console.log(weather)
+
+  let dateAccountForTimezone = new Date((new Date().getTime()) + weather.timezone * 1000)
+  let updatedLocalTime = dateAccountForTimezone.toISOString().match(/\d\d:\d\d/);
+
+  let weatherIconCode = weather.weather[0].icon;
+  // onClick={setCity(`Leipzig`)}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='wrapper'>
+        <button onClick={() => setCityName("Leipzig")}>Leipzig</button>
+        <button onClick={() => setCityName("München")}>München</button>
+        <button onClick={() => setCityName("Wien")}>Wien</button>
+        <button onClick={() => setCityName("Hamburg")}>Hamburg</button>
+        <p className='weatherOverview'>{weather.weather[0].description} in {weather.name}</p>
+        <img src={`http://openweathermap.org/img/wn/${weatherIconCode}@2x.png`} alt="current weather" />
+        <p>Current: {weather.main.temp}°C</p>
+        <p>Wind speed: {weather.wind.speed} km/h</p>
+        <p>Local time: {updatedLocalTime}</p>
+      </div>
     </div>
   );
 }
